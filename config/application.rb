98eba16d3acdeb8ea186e22aa8eb3f1ce8a346c4
DaveBett <davide.betterini@gmail.com>
2026-03-23 10:55:03 +0000

    Added system tests

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module StoreTest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators.test_framework false
   end
 end
